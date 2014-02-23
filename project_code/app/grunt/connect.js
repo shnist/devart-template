aaba1aef7ea98173d7be7f979ccff795da073d6e
shnist <aaron.jack.faber@gmail.com>
2014-02-23 12:48:19 +0000

    Added watch task for less compilation, which has resolved the keepalive issue with the connect server

--- project_code/app/grunt/connect.js
+++ project_code/app/grunt/connect.js
@@ -4,7 +4,6 @@ module.exports = {
 			port: 3010,
 			hostname: 'localhost',
 			base: 'www',
-			keepalive: true,
 			open: {
 				target: 'http://localhost:3010',
 				appName: 'chromium-browser'
---
+++ project_code/app/grunt/watch.js
@@ -0,0 +1,9 @@
+module.exports = {
+	less: {
+		files: ['www/less/*.less'],
+		tasks: ['less'],
+		options: {
+			livereload: true
+		}
+	}
+};
